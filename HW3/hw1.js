8b2d42c59aa81125e3abac7a8541fdf2311f6411
denys <maxkuz28011@gmail.com>
2023-08-26 21:23:50 +0300

    hw3

---
+++ HW3/hw1.js
@@ -0,0 +1,37 @@
+for (let index = 0; index < 10; index++){
+    document.write('<div>forem</div>')
+}
+
+for (let index = 0; index < 10; index++) {
+    document.write(`<div>${Text1} - forem 2 </div>`) ;
+}
+
+
+let x = 0;
+
+    while (x <= 20) {
+        document.write(`<h1>Hello</h1>`)
+        x++;
+    };
+
+let xc = 0;
+    while (xc <= 20) {
+        document.write(`<h1>${Goodbay}Text</h1>`)
+        xc++;
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
